f7bd5a96a900d94dc9665551ed5c096c4974fc37
BuildTools <unconfigured@null.spigotmc.org>
2025-06-14 12:43:31 +0100

    Fix: use azure-provided Port enviroment variable in flask app

--- docker/app/app.py
+++ docker/app/app.py
@@ -32,6 +32,9 @@ def home():
     </html>
     '''
 
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
